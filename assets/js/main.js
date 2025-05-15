a59ff2d78ab1a11022993a4cc52d93212c6de92d
William Rusiecki <146844019+ItzScarr@users.noreply.github.com>
2025-05-15 01:37:47 +0100

    s

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@
 console.log('Three.js and GLTFLoader loaded successfully');
 console.log('main.js loaded');
 
- import * as THREE from "../node_modules/three/build/three.module.js
+ import * as THREE from "../three.module.js"
 import { GLTFLoader } from './GLTFLoader.js';
 import { OrbitControls } from './OrbitControls.js';
 
